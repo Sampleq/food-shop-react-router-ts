c45dadff3f8c62409c303e9b34b6fce4954b3061
Igor M. <samplefines@gmail.com>
2025-08-10 15:01:17 +0300

    Updates

---
+++ assets/index-Jq6PK8Sj.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-Ckb7cISK.js";const t={},s=({})=>o.jsx("div",{className:t.notFound,children:"NotFound Component"});export{s as NotFound,s as default};
--- assets/index-SheaNsm4.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-Dnl-8W54.js";const t={},s=({})=>o.jsx("div",{className:t.notFound,children:"NotFound Component"});export{s as NotFound,s as default};
